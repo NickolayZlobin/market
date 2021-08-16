31aa2fa7d4ddc330f661d10fd3f25347c8dbb240
Nickolay L. Zlobin <nickolay-z@yandex.ru>
2021-08-16 12:28:23 +0300

    version 1.1

--- main.rb
+++ main.rb
@@ -11,6 +11,24 @@ require_relative "lib/product"
 require_relative "lib/book"
 require_relative "lib/movie"
 
-mask = Movie.new(price: 290, amount: 4)
+products = []
 
-puts "Фильм Маска стоит #{mask.price} руб, в наличии #{mask.amount} шт."
+products << Film.new(
+  title: 'Леон', year: '1994', director: 'Люк Бессон', price: 990, amount: 5
+)
+
+products << Film.new(
+  title: 'Дурак', year: '2014', director: 'Юрий Быков', price: 390, amount: 1
+)
+
+products << Book.new(
+  title: 'Идиот',
+  genre: 'роман',
+  author: 'Федор Достоевский',
+  price: 1500,
+  amount: 10
+)
+
+puts 'Вот какие товары у нас есть:'
+puts
+products.each { |product| puts product }
